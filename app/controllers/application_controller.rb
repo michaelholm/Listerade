2183b008405138ae438fed0ebe8749e2d6c477ea
Michael Holm <holm@whatismtheory.com>
2011-10-21 18:29:49 -0500

    added challenge/response password protect for dev

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+
+  USER_NAME, PASSWORD = "breos", "breos"
+  before_filter :authenticate_testing
+  
+  def authenticate_testing
+   if Rails.env.development?
+    authenticate_or_request_with_http_basic do |user_name, password|
+      user_name == USER_NAME && password == PASSWORD
+    end
+   end
+ end
   
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
