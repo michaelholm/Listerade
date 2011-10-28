ed5a61b87ba3139b5c6ff698b94febc06d1793a8
Michael Holm <holm@whatismtheory.com>
2011-10-28 09:55:24 -0500

    updated sendgrid creds

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,13 @@ Mongo::Application.initialize!
 
 
 require 'will_paginate'
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => "app1097050@heroku.com",
+  :password => "k3ucnfee",
+  :domain => "stark-sunset.heroku.com",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
