46147d27cbd2739c2cc150f836957a8d1d7b2a6a
Michael Holm <holm@whatismtheory.com>
2011-11-04 17:23:59 -0500

    added auth before..

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,25 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_user!
+  
   def dashboard
     @user = current_user   #User.find(params[:id])
     # If this show page is only for the currently logged in user change it to @user = current_user
+  end
+  
+  def add_fave(new_fave)
+  	if user_signed_in?
+  		current_user.fave_listings << new_fave
+  		current_user.save
+  	end
+  end
+  
+  def save_search()
+  	if user_signed_in?
+  		# save the search
+  		# json = params.to_json
+  		# ss = SavedSearch.find_or_create_by_query(json)
+  		# ss.user = current_user
+  		# ss.save
+  	end
   end
 end
