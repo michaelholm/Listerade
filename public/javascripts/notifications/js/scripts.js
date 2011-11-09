ba1fec025142c407e5679791d92bd00bb85f95f3
Michael Holm <holm@whatismtheory.com>
2011-11-09 09:04:25 -0600

    added js and style for notifications

---
+++ public/javascripts/notifications/js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+
+	// Closing Divs - used on Notification Boxes
+	$(".canhide").click(function() {
+		
+		//$(this).text('');
+		//$(this).removeClass('');
+		//$(this).addClass('notifications').addClass('canhide');
+		
+		$(this).fadeOut(800);
+	});
+	
+// Closing jQuery
+});
---
+++ public/stylesheets/notifications/js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+
+	// Closing Divs - used on Notification Boxes
+	$(".canhide").click(function() {
+		
+		$(this).fadeOut(600);
+	});
+	
+// Closing jQuery
+});
