ee4d58b2837f2b62b69479a8b7abd620167fe8f9
Michael Holm <michael.holm@gmail.com>
2017-04-18 16:49:49 -0500

    Removed old db name and uri

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,8 +1,11 @@
 #require 'mongo_mapper' # loading mongo_mapper
 
+#Choose a connection
 #MongoMapper.connection = Mongo::Connection.new('localhost', 27017, :logger => Rails.logger)
-MongoMapper.connection = Mongo::Connection.from_uri('mongodb://heroku_app1097050:kfi5tr5lf2eculchdnbn8chlvv@dbh63.mongolab.com:27637/heroku_app1097050', :logger => Rails.logger)
-MongoMapper.database = "heroku_app1097050"
+#MongoMapper.connection = Mongo::Connection.from_uri('', :logger => Rails.logger)
+
+#SET database name
+#MongoMapper.database = ""
 
 if defined?(PhusionPassenger)
    PhusionPassenger.on_event(:starting_worker_process) do |forked|
